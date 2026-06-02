7b4123b48b57021bec54e8a16df8692730b3e611
Ata <atamusawer@gmail.com>
2026-06-02 20:58:00 +0800

    Fix: Update Firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -1,19 +1,22 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCLqhL1rthcfpuY9Q54jdOC_EYRTH0ViRc",
-  authDomain: "clean-chat-38c26.firebaseapp.com",
-  projectId: "clean-chat-38c26",
-  storageBucket: "clean-chat-38c26.appspot.com",
-  messagingSenderId: "708275902324",
-  appId: "1:708275902324:web:e39861f7ee179fea07fe21",
+  apiKey: "AIzaSyBMb5Yf1PWg31D1PLb8F72TR0pvan4GtoM",
+  authDomain: "world-message-c1c4d.firebaseapp.com",
+  databaseURL: "https://world-message-c1c4d-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "world-message-c1c4d",
+  storageBucket: "world-message-c1c4d.firebasestorage.app",
+  messagingSenderId: "817536129329",
+  appId: "1:817536129329:web:ffd959c0ff04a0b498b810",
+  measurementId: "G-KQLV5GM85T"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-export const auth = getAuth(app);
-export const db = getFirestore(app);
-export const storage = getStorage(app);
+const analytics = getAnalytics(app);
