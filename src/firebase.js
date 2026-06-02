961bbded2ec7c1a0e3169fd346cd4fa1d062e9bd
Ata <atamusawer@gmail.com>
2026-06-02 21:00:48 +0800

    Fix: Complete Firebase setup with Auth, Firestore, and Storage

--- src/firebase.js
+++ src/firebase.js
@@ -1,22 +1,19 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBMb5Yf1PWg31D1PLb8F72TR0pvan4GtoM",
   authDomain: "world-message-c1c4d.firebaseapp.com",
-  databaseURL: "https://world-message-c1c4d-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "world-message-c1c4d",
-  storageBucket: "world-message-c1c4d.firebasestorage.app",
+  storageBucket: "world-message-c1c4d.appspot.com",
   messagingSenderId: "817536129329",
   appId: "1:817536129329:web:ffd959c0ff04a0b498b810",
-  measurementId: "G-KQLV5GM85T"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
